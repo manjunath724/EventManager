778894aa626de1bb01ca8c7c1eb3858e73cddd68
manjunath724 <manjunath.p@nighthack.in>
2018-10-07 12:43:51 +0530

    User Management

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |t|
+  name = Faker::Internet.username
+  email = Faker::Internet.safe_email
+  pass = Faker::Internet.password
+  phone = Faker::PhoneNumber.cell_phone
+  user = User.create(name: name, email: email, phone: phone, password: pass, password_confirmation: pass)
+
+  p "#{t + 1}. Created: #{user.inspect}"
+end
